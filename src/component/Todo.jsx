d7a2ff78cd197021085ca4b0930e728cbe93faa4
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2022-05-26 00:17:10 +0530

    add oagination

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,26 +1,60 @@
 import React, { useEffect, useState } from 'react'
 
 const Todo = () => {
-    const [todos,settodos]=useState([])
+    const [todos,settodos]=useState([]);
+    const [newtodo,setnewtodo]=useState("");
+    const [count,setcount]=useState(0)
+    let saveinfo=()=>{
+        //call api to save this information to backend
+        fetch('http://localhost:8080/todos',{
+            // post means send data to server means db.json file
+            method:"POST",
+            headers:{
+                "content-type":"application/json",
+
+            },
+            body:JSON.stringify({
+                text:newtodo, "isComplated":false,
+
+            })
+           
+
+        }) 
+        // this code for show data in body without refracing
+         .then((response) => response.json())
+        .then((data) =>{settodos([...todos,data])
+             setnewtodo("");
+        })
+    };
    
     useEffect(()=>{
-        fetch('http://localhost:8080/todos')
-        .then(response => response.json())
-        .then(data => settodos(data));
+        const getdata=()=>{
+            fetch(`http://localhost:8080/todos?_page=${count}&_limit=7`)
+            .then(response => response.json())
+            .then(data => settodos(data));
 
-    },[])
+        }
+        getdata();
+       
+
+    },[count])
   return (
     <div>Todo
         <div>
             <div>
-            <input type="text" />
-            <button>+</button>
+            <input type="text" onChange={(e)=>setnewtodo(e.target.value)} />
+            <button onClick={saveinfo}>+</button>
             </div>
             {todos.map((el)=>(
-                <div key={el.id}>{el.value}</div>
+                <div key={el.id}>{el.text}</div>
 
             ))}
         </div>
+        <div>
+           <button disabled ={count<=1} onClick={()=>setcount(count-1)}>pre</button>
+           <button onClick={()=>setcount(count+1)}>next</button>
+        </div>
+        <div>{count}</div>
         
       
     </div>
